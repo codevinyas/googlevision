ae461d9557b0565e6a02b2412b6d3b1be35552a8
codevinyas <vinyas_r@yahoo.com>
2018-10-08 23:12:10 -0400

    first

---
+++ GoogleVision/Program.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleVision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates a client
+            var client = ImageAnnotatorClient.Create();
+            // Load the image file into memory
+            var image = Image.FromFile(@"D:\Work\Learning\Cloud\Google\GoogleVision\Data\wf.jpg");
+            // Performs label detection on the image file
+            var response = client.DetectLogos(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
+            response = client.DetectText(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
+            Console.Write("complete");
+            Console.Read();
+        }
+    }
+}
